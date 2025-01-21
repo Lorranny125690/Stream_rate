3205da15198b75b5e6e00c265d49e9cd84721ce5
Lorrapaz <lorrannyyasmin62gmail.com>
2025-01-21 10:25:27 -0300

    test

--- Stream_rate/src/Interfaces/Exibivel.java
+++ Stream_rate/src/Interfaces/Exibivel.java
@@ -3,4 +3,5 @@ package Interfaces;
 public interface Exibivel {
     String exibirDetalhes(String nome, double nota, String genero, int ano, String tipo, String idioma);
     String exibirPrevia(String descricao);
+    //socorro
 }
